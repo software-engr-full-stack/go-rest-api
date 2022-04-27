4cb162488007a4f04e4384953bddabe752987a3e
michael <software.engr.full.stack@gmail.com>
2022-04-26 18:23:01 -0700

    Remove dump option for sqlite testing

--- sqlite/sqlite_test.go
+++ sqlite/sqlite_test.go
@@ -1,15 +1,10 @@
 package sqlite_test
 
 import (
-	"flag"
-	"io/ioutil"
-	"path/filepath"
 	"testing"
 
 	"go-rest-api/sqlite"
 )
-
-var dump = flag.Bool("dump", false, "save work data")
 
 // Ensure the test database can open & close.
 func TestDB(t *testing.T) {
@@ -24,14 +19,6 @@ func MustOpenDB(tb testing.TB) *sqlite.DB {
 	// Write to an in-memory database by default.
 	// If the -dump flag is set, generate a temp file for the database.
 	dsn := ":memory:"
-	if *dump {
-		dir, err := ioutil.TempDir("", "")
-		if err != nil {
-			tb.Fatal(err)
-		}
-		dsn = filepath.Join(dir, "db")
-		println("DUMP=" + dsn)
-	}
 
 	db := sqlite.NewDB(dsn)
 	if err := db.Open(); err != nil {
